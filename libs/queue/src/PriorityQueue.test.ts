93ade0174bc01660f0f01433a451d69f04e71ca7
Aadil Mehraj <aadilmehraj76@gmail.com>
2024-02-11 15:14:06 +0530

    Cleanup and update UT

--- libs/queue/src/PriorityQueue.test.ts
+++ libs/queue/src/PriorityQueue.test.ts
@@ -4,10 +4,17 @@ import { PriorityQueue } from './PriorityQueue.js';
 
 const comparator = (a: string, b: string) => a.localeCompare(b);
 
-test('should return the instance of Queue', (t) => {
+test('should return the instance of PriorityQueue', (t) => {
   const queue = new PriorityQueue(comparator);
 
   t.true(queue instanceof PriorityQueue);
+});
+
+test('should return the current default value', (t) => {
+  const queue = new PriorityQueue(comparator);
+
+  t.is(queue.size, 0);
+  t.deepEqual([...queue], []);
 });
 
 test('should enqueue item in the queue ', (t) => {
@@ -20,7 +27,7 @@ test('should enqueue item in the queue ', (t) => {
   t.deepEqual([...queue], ['1', '2']);
 });
 
-test.only('should enqueue item based on priority', (t) => {
+test('should enqueue item based on priority', (t) => {
   const queue = new PriorityQueue(comparator);
 
   queue.enqueue('4');
@@ -40,4 +47,54 @@ test.only('should enqueue item based on priority', (t) => {
 
   queue.enqueue('1');
   t.deepEqual([...queue], ['1', '2', '3', '4', '7', '8']);
+});
+
+test('should dequeue items in the queue', (t) => {
+  const queue = new PriorityQueue(comparator);
+
+  queue.enqueue('1');
+  queue.enqueue('3');
+  queue.enqueue('2');
+
+  t.deepEqual([...queue], ['1', '2', '3']);
+
+  t.is(queue.dequeue(), '1');
+  t.is(queue.size, 2);
+
+  t.is(queue.dequeue(), '2');
+  t.is(queue.size, 1);
+
+  t.is(queue.dequeue(), '3');
+  t.is(queue.size, 0);
+});
+
+test('should dequeue empty queue correctly', (t) => {
+  const queue = new PriorityQueue(comparator);
+
+  t.is(queue.size, 0);
+
+  t.is(queue.dequeue(), undefined);
+
+  t.is(queue.size, 0);
+});
+
+test('should clear the queue', (t) => {
+  const queue = new PriorityQueue(comparator);
+
+  queue.enqueue('1');
+  queue.enqueue('3');
+  queue.enqueue('2');
+
+  t.deepEqual([...queue], ['1', '2', '3']);
+
+  queue.clear();
+  t.is(queue.size, 0);
+});
+
+test('should clear the empty queue', (t) => {
+  const queue = new PriorityQueue(comparator);
+
+  t.is(queue.size, 0);
+  queue.clear();
+  t.is(queue.size, 0);
 });
--- libs/queue/src/PriorityQueue.ts
+++ libs/queue/src/PriorityQueue.ts
@@ -10,31 +10,35 @@ export class PriorityQueue<TItem> extends Queue<TItem> {
     this.#comparator = comparator;
   }
 
+  private hasPriority(itemA: Node<TItem>, itemB: Node<TItem>) {
+    return this.#comparator(itemA.value, itemB.value) > 0;
+  }
+
   enqueue(item: TItem): void {
     const node = new Node(item);
 
     if (this.head === null || this.tail === null) {
       this.head = this.tail = node;
+      this.size++;
       return;
     }
 
-    if (this.#comparator(this.head.value, node.value) > 0) {
+    if (this.hasPriority(this.head, node)) {
       node.next = this.head;
       this.head = node;
+      this.size++;
 
       return;
     }
 
     let current: Node<TItem> = this.head;
 
-    while (
-      current.next !== null &&
-      this.#comparator(current.next.value, node.value) <= 0
-    ) {
+    while (current.next !== null && !this.hasPriority(current.next, node)) {
       current = current.next;
     }
 
     node.next = current.next;
     current.next = node;
+    this.size++;
   }
 }
